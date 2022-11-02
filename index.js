771f0f7307de4ba57a3eee73c95e8662de7dd184
56_Kundan Kumar Yadav <kundanbth8538@gmail.com>
2022-11-02 06:03:58 +0000

    Update 2 files

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question('Please enter potential palindrome: ', checkForPalindrome);
+rl.question("Please enter potential palindrome: ", checkForPalindrome);
 
 function checkForPalindrome(s) {
-    if (s == s.split("").reverse().join("")) {
-        console.log(s, "is a Palindrome");
-    } else {
-        console.log(s, "Not a Palindrome");
-    }
+  if (s == s.split("").reverse().join("")) {
+    console.log(s, "is a Palindrome");
+  } else {
+    console.log(s, "is not a Palindrome");
+  }
 }
